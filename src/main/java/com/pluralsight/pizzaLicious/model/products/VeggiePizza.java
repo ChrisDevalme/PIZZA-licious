bc1c04e726d6c1348e6fbd444ad1401adc675f81
Chris Devalme <cdevalme@gmail.com>
2026-05-29 08:12:57 -0400

    - Folder restructure

--- src/main/java/com/pluralsight/pizzaLicious/model/products/VeggiePizza.java
+++ src/main/java/com/pluralsight/pizzaLicious/model/products/VeggiePizza.java
@@ -1,10 +1,8 @@
 package com.pluralsight.pizzaLicious.model.products;
 
-import com.pluralsight.model.topping.premiumTopping.*;
-import com.pluralsight.model.topping.regularTopping.*;
-import com.pluralsight.pizzaLicious.model.topping.premiumTopping.CheeseTopping;
-import com.pluralsight.pizzaLicious.model.topping.regularTopping.RegularTopping;
-import com.pluralsight.pizzaLicious.model.topping.regularTopping.SauceTopping;
+import com.pluralsight.pizzaLicious.model.topping.premiumTopping.*;
+import com.pluralsight.pizzaLicious.model.topping.regularTopping.*;
+
 
 public class VeggiePizza extends Pizza {
 
